d61b721a682eee83db586b027f8f30372ca30fd8
cenksari <cenk@cenksari.com>
2018-02-06 15:34:04 +0300

    Options section, styles and confirm dialog added.

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,9 +1,61 @@
-$(document).ready(function () {
-    startTime();
+let userName
+let weatherLocation;
 
+chrome.storage.sync.get({
+    userName: 'Chrome user',
+    weatherLocation: 'Istanbul, TR'
+}, function (items) {
+    userName = items.userName;
+    weatherLocation = items.weatherLocation;
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+    for (key in changes) {
+        var storageChange = changes[key];
+
+        if (key == "userName") {
+            userName = storageChange.newValue;
+
+            startLocalTime();
+        }
+
+        if (key == "weatherLocation") {
+            weatherLocation = storageChange.newValue;
+
+            getWeatherFromApi();
+        }
+    }
+});
+
+$(document).ready(function () {
+    startLocalTime();
+
+    getWeatherFromApi();
+
+    getBookmarksFromFile();
+
+    let isMenuOpened = false;
+
+    $(".menu a").click(function (e) {
+        e.preventDefault();
+
+        if (isMenuOpened) {
+            $("ul").hide();
+
+            isMenuOpened = false;
+        }
+        else {
+            $("ul").show();
+
+            isMenuOpened = true;
+        }
+    });
+});
+
+function getWeatherFromApi() {
     const getWeather = new Promise(function (resolve, reject) {
         $.simpleWeather({
-            location: 'Istanbul, TR',
+            location: weatherLocation,
             woeid: '',
             unit: 'c',
             success: function (weather) {
@@ -16,11 +68,13 @@ $(document).ready(function () {
     });
 
     getWeather.then(function (weather) {
-        $("#weather").html('Istanbul, TR <i class="material-icons">cloud_queue</i> <strong>' + weather.temp + '&deg;' + weather.units.temp + '</strong>');
+        $("#weather").html('' + weatherLocation + ' <i class="material-icons">cloud_queue</i> <strong>' + weather.temp + '&deg;' + weather.units.temp + '</strong>');
     }).catch(function (error) {
         $("#weather").html(error);
     });
+}
 
+function getBookmarksFromFile() {
     const getBookmarks = new Promise(function (resolve, reject) {
         $.ajax({
             url: chrome.extension.getURL('bookmarks.json'),
@@ -49,27 +103,11 @@ $(document).ready(function () {
     }).catch(function (error) {
         $(".menu").html(error);
     });
+}
 
-    let isMenuOpened = false;
-
-    $(".menu a").click(function (e) {
-        e.preventDefault();
-
-        if (isMenuOpened) {
-            $("ul").hide();
-
-            isMenuOpened = false;
-        }
-        else {
-            $("ul").show();
-
-            isMenuOpened = true;
-        }
-    });
-});
-
-function startTime() {
+function startLocalTime() {
     let today = new Date();
+
     let h = today.getHours();
     let m = today.getMinutes();
 
@@ -77,7 +115,7 @@ function startTime() {
 
     $("#time").text(h + ":" + m);
 
-    let t = setTimeout(startTime, 500);
+    let t = setTimeout(startLocalTime, 500);
 
     let salutation = '';
 
@@ -94,7 +132,7 @@ function startTime() {
         salutation = "Good evening";
     }
 
-    $("#salute").text(salutation + ', Cenk');
+    $("#salute").text(salutation + ', ' + userName);
 }
 
 function checkTime(i) {
--- scripts/options.js
+++ scripts/options.js
@@ -1,4 +1,79 @@
+chrome.storage.sync.get({
+    userName: 'Chrome user',
+    weatherLocation: 'Istanbul, TR'
+}, function (items) {
+    $("#name").val(items.userName);
+    $("#location").val(items.weatherLocation);
+});
+
 $(document).ready(function () {
+    getBookmarksFromFile();
+
+    $(document).on("click", ".delete", function (e) {
+        let link = $(this).attr("data-id");
+
+        $.confirm({
+            title: 'Confirm!',
+            content: 'Are you sure you want to delete item?',
+            type: 'red',
+            boxWidth: '400px',
+            useBootstrap: false,
+            typeAnimated: true,
+            buttons: {
+                delete: {
+                    text: 'Yes, Delete',
+                    btnClass: 'btn-red',
+                    action: function () {
+
+                    }
+                },
+                close: function () {
+                }
+            }
+        });
+    });
+
+    $("#options").submit(function (e) {
+        e.preventDefault();
+
+        let name = $.trim($("#name").val());
+        let location = $("#location").val();
+
+        if (name == "") {
+            $.alert({
+                title: 'Error!',
+                content: 'Please enter your name.',
+                type: 'red',
+                boxWidth: '400px',
+                useBootstrap: false,
+                typeAnimated: true,
+            });
+        }
+        else {
+            chrome.storage.sync.set({
+                userName: name,
+                weatherLocation: location
+            }, function () {
+                $.alert({
+                    title: 'Success!',
+                    content: 'Options successfully saved.',
+                    type: 'blue',
+                    boxWidth: '400px',
+                    useBootstrap: false,
+                    typeAnimated: true,
+                    autoClose: 'okButton|9000',
+                    buttons: {
+                        okButton: {
+                            text: "OK"
+                        }
+                    }
+                });
+            });
+        }
+    });
+});
+
+function getBookmarksFromFile() {
     const getBookmarks = new Promise(function (resolve, reject) {
         $.ajax({
             url: chrome.extension.getURL('bookmarks.json'),
@@ -18,7 +93,7 @@ $(document).ready(function () {
         $.each(bookmarks, function (i, bookmark) {
             countBookmarks++;
 
-            $("table").append('<tr><td>' + bookmark.name + '</td><td><a href="' + bookmark.link + '">' + bookmark.link + '</a></td><td><button type="button" class="delete"><i class="material-icons">delete</i></button></td></tr>');
+            $("table").append('<tr><td>' + bookmark.name + '</td><td><a href="' + bookmark.link + '">' + bookmark.link + '</a></td><td><button type="button" class="delete" data-id="' + bookmark.link + '"><i class="material-icons">delete</i></button></td></tr>');
         });
 
         if (countBookmarks < 1) {
@@ -27,6 +102,4 @@ $(document).ready(function () {
     }).catch(function (error) {
         $("#error-message").html(error);
     });
-
-    
-});
+}
